6d4756cd2cc5df1e4dd28dbfd2d611c7a8189d07
hersh <h.nevgi@gmail.com>
2020-06-01 14:30:53 +1000

    feat(i3bar): battery and spotifycli tweaks

--- .config/i3/i3b_battery.sh
+++ .config/i3/i3b_battery.sh
@@ -7,6 +7,10 @@
 chg_percent=$(upower -i $(upower -e | grep Display) | grep --color=never -E percentage|xargs|cut -d' ' -f2 | cut -d'.' -f1)
 chg_status=$(upower -i $(upower -e | grep Display) | grep -E state|xargs|cut -d' ' -f2)
 
+if [ "$chg_percent" == "100%" ]; then
+	chg_percent=100
+fi
+
 if [ "$chg_status" == "charging" ] || [ "$chg_status" == "fully-charged" ]; then
 	icon=
 elif [ "$chg_percent" -ge 90 ]; then
@@ -22,13 +26,9 @@ else
 	icon=
 fi
 
-if [ "$chg_percent" == "100%" ]; then
-	chg_percent=100
-fi
-
 echo $icon $chg_percent%
 echo $icon $chg_percent%
-if [ "$chg_percent" -le 15 ] && [ "$chg_status" == "discharging" ]; then
+if [ "$chg_percent" -le 25 ] && [ "$chg_status" == "discharging" ]; then
 	echo "#ff8000";
 else
 	echo "#ffffff";
